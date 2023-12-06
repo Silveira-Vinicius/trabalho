e54791111c629c55e308f1f81aa0863daa8cb7fe
Vinicius Silveira <vinicius.silveira@sou.unifal-mg.edu.br>
2023-12-05 23:36:16 -0300

    Initial commit

---
+++ src/main/java/com/example/trab/controller/CrudController.java
@@ -0,0 +1,10 @@
+package com.example.trab.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class CrudController {
+
+}
